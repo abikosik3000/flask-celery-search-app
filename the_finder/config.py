4b26666c1b2d888bf34470fbd8f12da04a9424a9
abikosik3000 <Padla-Oleg3000@yandex.ru>
2023-04-20 23:18:59 +0300

    some spases

--- the_finder/config.py
+++ the_finder/config.py
@@ -5,17 +5,17 @@ from the_finder import app
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["BASEDIR"] = basedir
 
-#Директория для поиска файлов
+# Директория для поиска файлов
 app.config["SEARCH_DIRECTORY"] = "/home/abikosik3000/var/www/search_app/test_repo"
 
-#REDIS
+# REDIS
 app.config["REDIS_HOST"] = '6379'
 app.config["REDIS_PORT"] = '6379'
 app.config["REDIS_DB_NUM"] = '0'
 app.config["REDIS_USER"] = None
 app.config["REDIS_PASS"] = None
 
-#CELERY
+# CELERY
 app.config["CELERY_BROKER"] = "redis://localhost"
 app.config["CELERY_BACKEND"] = None
 
